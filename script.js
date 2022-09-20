8ff1d808c75c2479cded135a65e883e9e037a1f5
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-20 21:04:26 +0000

    added function to remove book from array

--- script.js
+++ script.js
@@ -4,25 +4,41 @@ const addBtn = document.querySelector('.addBtn');
 
 //Get data from localstorage
 const parsedData = JSON.parse(localStorage.getItem(('books')));
-const books = parsedData;
+const books = parsedData ? parsedData : [];
 
 // Display books on website
 function displayBooks() {
-    books.forEach((item) => {
-        addBooktoArray(item);
+    books.forEach((item, index) => {
+        addBooktoArray(item, index);
     });
 }
 
 //Add book to browser
-function addBooktoArray(item) {
+function addBooktoArray(item, index) {
     const li = document.createElement('li');
     li.classList.add('list-item');
+    li.setAttribute('key', index);
     li.innerHTML = `
     <h3 class="title">${item.title}</h3>
     <p class="author">${item.author}</p>
     <button class="removeBtn">Remove</button><hr>`;
 
     ul.appendChild(li);
+}
+
+//remove book
+function removeBook(element) {
+    if(element.classList.contains('removeBtn')) {
+        element.parentElement.remove();
+        let bookID = element.parentElement.getAttribute('key');
+        const result = books.filter((book) => {
+          return book !== books[bookID];
+        });
+
+        //convert result into string and store new data in localStorage
+        let filteredData = JSON.stringify(result);
+        localStorage.setItem('books', filteredData);
+    }
 }
 
 //Event to display books in browser
@@ -40,17 +56,26 @@ form.addEventListener('submit', (e) => {
     const titleValue = title.value;
     const authorValue = author.value;
 
-    //create book
-    const book = {
+    //add book when fields are not empty
+    if(titleValue !== '' || authorValue !== '') {
+      //create book object
+      const book = {
         title: titleValue,
         author: authorValue
-    };
+      };
 
-    //add book object to array
-    books.push(book);
-    addBooktoArray(book);
+      //add book object to array
+      books.push(book);
+      addBooktoArray(book);
 
-    //convert books array into string
-    const booksStr = JSON.stringify(books);
-    localStorage.setItem('books', booksStr);
+      //convert books array into string and store in localStorage
+      const booksStr = JSON.stringify(books);
+      localStorage.setItem('books', booksStr);
+      form.reset();
+    }
+});
+
+//Event to remove book
+ul.addEventListener('click', (e) => {
+    removeBook(e.target);
 });
