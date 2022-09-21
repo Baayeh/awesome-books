60caed446f48c98e129a655cf16a3a1311825e16
Joel-100 <joelkugonza@gmail.com>
2022-09-21 17:26:34 +0300

    Use classes to add and remove books

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
 const form = document.querySelector('#form');
 const ul = document.querySelector('#book-list');
 
-// Get data from localstorage
-const parsedData = JSON.parse(localStorage.getItem(('books')));
-const books = parsedData;
-
-// Add book to browser
-function addBooktoArray(item, index) {
-  const li = document.createElement('li');
-  li.classList.add('list-item');
-  li.setAttribute('key', index);
-  li.innerHTML = `
-  <h3 class="title">${item.title}</h3>
-  <p class="author">${item.author}</p>
-  <button class="removeBtn">Remove</button><hr>`;
-
-  ul.appendChild(li);
+// if books !== null
+let books;
+if(localStorage.getItem('books') === null) {
+  books = [];
+} else {
+  books = JSON.parse(localStorage.getItem('books'));
 }
+
+// Define Book Class
+class Book {
+  constructor(title, author) {
+    this.title = title; // let title = title
+    this.author = author; // let author = author
+  }
+}
+
+// Define Methods class
+class methods { 
+  // Add book to browser
+  addBooktoArray(item, index) {
+    const li = document.createElement('li');
+    li.classList.add('list-item');
+
+    // add key attribute with value of index (key = index)
+    li.setAttribute('key', index);
+    li.innerHTML = `
+    <h3 class="title">${item.title}</h3>
+    <p class="author">${item.author}</p>
+    <button class="removeBtn">Remove</button><hr>`;
+
+    ul.appendChild(li);
+  }
+
+  // remove book
+ removeBook(element) {
+  if (element.classList.contains('removeBtn')) {
+    // get parent element of button and remove it from the browser
+      element.parentElement.remove();
+
+      const bookID = element.parentElement.getAttribute('key');
+      const result = books.filter((book) => book !== books[bookID]);
+
+      // convert result into string and store new data in localStorage
+      const filteredData = JSON.stringify(result);
+      localStorage.setItem('books', filteredData);
+    }
+  }
+}
+
+//Create an instance of methods class
+const method = new methods();
 
 // Display books on website
 function displayBooks() {
   books.forEach((item, index) => {
-    addBooktoArray(item, index);
+    method.addBooktoArray(item, index);
   });
-}
-
-// remove book
-function removeBook(element) {
-  if (element.classList.contains('removeBtn')) {
-    element.parentElement.remove();
-    const bookID = element.parentElement.getAttribute('key');
-    const result = books.filter((book) => book !== books[bookID]);
-
-    // convert result into string and store new data in localStorage
-    const filteredData = JSON.stringify(result);
-    localStorage.setItem('books', filteredData);
-  }
 }
 
 // Event to display books in browser
@@ -55,14 +77,11 @@ form.addEventListener('submit', (e) => {
   // add book when fields are not empty
   if (titleValue !== '' || authorValue !== '') {
     // create book object
-    const book = {
-      title: titleValue,
-      author: authorValue,
-    };
-
+    const book = new Book(titleValue, authorValue);
+    
     // add book object to array
     books.push(book);
-    addBooktoArray(book);
+    method.addBooktoArray(book);
 
     // convert books array into string and store in localStorage
     const booksStr = JSON.stringify(books);
@@ -73,5 +92,6 @@ form.addEventListener('submit', (e) => {
 
 // Event to remove book
 ul.addEventListener('click', (e) => {
-  removeBook(e.target);
+  // getting element that is clicked in the ul
+  method.removeBook(e.target);
 });
