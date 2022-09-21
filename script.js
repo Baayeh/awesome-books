ab126bb7a2f69f224974114da3ece4c11ef4823b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-21 19:35:49 +0000

    Revert "Using Classes for awesome Books"

--- script.js
+++ script.js
@@ -1,82 +1,97 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable class-methods-use-this */
-
 const form = document.querySelector('#form');
 const ul = document.querySelector('#book-list');
 
+// if books !== null
+let books;
+if(localStorage.getItem('books') === null) {
+  books = [];
+} else {
+  books = JSON.parse(localStorage.getItem('books'));
+}
+
+// Define Book Class
 class Book {
-  constructor(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
+  constructor(title, author) {
+    this.title = title; // let title = title
+    this.author = author; // let author = author
   }
 }
 
-class Methods {
- static books = [];
+// Define Methods class
+class methods { 
+  // Add book to browser
+  addBooktoArray(item, index) {
+    const li = document.createElement('li');
+    li.classList.add('list-item');
 
- static getAllBooks() {
-   if (localStorage.getItem('books')) {
-     this.books = JSON.parse(localStorage.getItem('books'));
-     this.displayBooks();
-   } else {
-     ul.style.display = 'none';
-   }
- }
+    // add key attribute with value of index (key = index)
+    li.setAttribute('key', index);
+    li.innerHTML = `
+    <h3 class="title">${item.title}</h3>
+    <p class="author">${item.author}</p>
+    <button class="removeBtn">Remove</button><hr>`;
 
- static displayBooks() {
-   this.books.forEach((book) => {
-     this.addToBookArray(book);
-   });
- }
+    ul.appendChild(li);
+  }
 
- static addToBookArray(book) {
-   const li = `<li class="list-item" key="${book.id}">
-  <p class="title">"${book.title}" by ${book.author}</p>
-  <button class="removeBtn">Remove</button>
-  </li>`;
-   ul.innerHTML += li;
- }
+  // remove book
+ removeBook(element) {
+  if (element.classList.contains('removeBtn')) {
+    // get parent element of button and remove it from the browser
+      element.parentElement.remove();
 
- static addBook(book) {
-   this.books.push(book);
-   const strData = JSON.stringify(this.books);
-   localStorage.setItem('books', strData);
- }
+      const bookID = element.parentElement.getAttribute('key');
+      const result = books.filter((book) => book !== books[bookID]);
 
- static removeBook(bookID) {
-   const result = this.books.filter((book) => String(bookID) !== String(book.id));
-   const strData = JSON.stringify(result);
-   localStorage.setItem('books', strData);
-   this.books = JSON.parse(localStorage.getItem('books'));
-   if (this.books.length !== 0) {
-     ul.style.display = 'block';
-   } else {
-     ul.style.display = 'none';
-     localStorage.clear();
-   }
- }
+      // convert result into string and store new data in localStorage
+      const filteredData = JSON.stringify(result);
+      localStorage.setItem('books', filteredData);
+    }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', Methods.getAllBooks());
+//Create an instance of methods class
+const method = new methods();
 
+// Display books on website
+function displayBooks() {
+  books.forEach((item, index) => {
+    method.addBooktoArray(item, index);
+  });
+}
+
+// Event to display books in browser
+document.addEventListener('DOMContentLoaded', displayBooks);
+
+// Event to Add a book to the array
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  // Getting input fields from the browser
   const [title, author] = form.elements;
+
+  // getting the values of both input fields
   const titleValue = title.value;
   const authorValue = author.value;
-  const book = new Book(titleValue, authorValue, Methods.books.length);
-  ul.style.display = 'block';
-  Methods.addBook(book);
-  Methods.addToBookArray(book);
-  form.reset();
+
+  // add book when fields are not empty
+  if (titleValue !== '' || authorValue !== '') {
+    // create book object
+    const book = new Book(titleValue, authorValue);
+    
+    // add book object to array
+    books.push(book);
+    method.addBooktoArray(book);
+
+    // convert books array into string and store in localStorage
+    const booksStr = JSON.stringify(books);
+    localStorage.setItem('books', booksStr);
+    form.reset();
+  }
 });
 
+// Event to remove book
 ul.addEventListener('click', (e) => {
-  if (e.target.classList.contains('removeBtn')) {
-    const bookID = e.target.parentElement.getAttribute('key');
-    Methods.removeBook(bookID);
-    e.target.parentElement.remove();
-  }
+  // getting element that is clicked in the ul
+  method.removeBook(e.target);
 });
